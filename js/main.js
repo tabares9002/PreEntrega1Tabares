1826eeb7a60fb38bba46a790c9ab56eda7f9c47b
tabares9002 <tabares9002@gmail.com>
2023-05-15 21:59:03 -0300

    Cambio el codigo

--- js/main.js
+++ js/main.js
@@ -1,41 +1,19 @@
-//ENTRADA DE DATOS
-alert ("Venta de Remeras $250 cada una")
+let nombre = prompt("Ingrese su nombre:");
+let apellido = prompt("Ingrese su Apellido:");
+let HorasDeTrabajo = parseInt(prompt("Ingrese las Horas Trabajadas en la Semana (Hasta 40hrs):"));
+let salarioHora = parseInt(prompt("Ingrese el monto de su Salario por Hora:"));
+let HorasExtras = (prompt("Trabajó Horas Extras? (Si/No):"));
 
-const IVA = 21;
-let precio = 250;
-let descuento = 0;
-
-
-let cantidad = 0;
-
-while ((cantidad <= 0) || (cantidad > 10)) {
-    cantidad = parseInt(prompt("Ingrese la cantida de productos (Hasta 10 productos):"));
+while (HorasDeTrabajo > 40) {
+    HorasDeTrabajo = parseInt(prompt("Error! Ingrese Nuevamente las Horas Trabajadas en la Semana (Hasta 30hrs):"));
 }
 
-let plazo = 0;
+// if (HorasExtras == Si) {
+//     let CantidadHorasExtras = parseInt(prompt("Ingrese Cantidad de Horas Extras Trabajadas:"));
+// }
 
-while ((plazo != 4) && (plazo != 6) && (plazo != 12)){
-    plazo = parseInt(prompt("Ingrese la cantida de cuotas (4/6/12):"));
-}
+let SalarioTotal = (HorasDeTrabajo * salarioHora)*4;
+alert("Sr/a" + " " + nombre + " " + apellido + "\nSu salario mensual es" + " " + "$" + SalarioTotal);
 
-switch (plazo) {
-    case 4:
-        descuento = 10;
-        break;
-    case 6:
-        descuento = 5;
-        break;
-    case 12:
-        descuento = 3;
-        break;
-    default:
-        break;
-}
-
-
-//CALCULOS
-
-function calculoDeDescuento(plazo, cantidad, precio){
-let precioInicial = (cantidad * precio);
-prompt (precioInicial + "Precio");
-}
+// function CalcularSalario()
+// CalcularSalario();
